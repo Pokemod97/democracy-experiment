d00931d2568d4484ffb6cc400e91fbcbc33f7269
Pokemod97 <cadenkline9@gmail.com>
2020-04-20 03:02:08 -0400

    New code

--- Voting.py
+++ Voting.py
@@ -1,9 +1,13 @@
-from discord.ext import commands
+from discord.ext import commands, tasks
+from github import Github
+import os
 
 
 class Voting(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.g = Github(os.getenv("github_token"))
+        self.repo = self.g.get_repo("Pokemod97/democracy-experiment")
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
@@ -16,6 +20,25 @@ class Voting(commands.Cog):
         print((member_count // 2) + 1)
         if reaction.count >= ((member_count // 2) + 1):
             print(reaction.emoji.name)
+
+    @commands.command()
+    async def info(self, ctx: commands.Context):
+        """   channelb = ctx.guild.get_channel(449750128073375775)
+            message = await channelb.fetch_message(671177470359240706)
+            print(message)
+            for x in message.embeds:
+                print(x.title)
+                print(x.description)
+                print(x.url)
+        """
+        
+    @commands.command()
+    async def hello(self, ctx: commands.Context):
+        await ctx.send("hello")
+
+    @commands.command()
+    async def r(self, ctx):
+        self.bot.reload()
 
 
 def setup(bot):
--- main.py
+++ main.py
@@ -4,7 +4,13 @@ import datetime
 import discord.ext.commands
 
 
-client = discord.ext.commands.Bot('$')
+class CustomBot(discord.ext.commands.Bot):
+
+    def reload(self):
+        self.reload_extension("Voting")
+
+
+client = CustomBot("$")
 
 
 @client.event
@@ -12,20 +18,5 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    print(message.author)
-    if str(message.author) == "GitHub#0000":
-        print(message.embeds[0].title)
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-    if message.content.startswith('$reload'):
-        print("reloading Voting")
-        client.reload_extension('Voting')
-
-client.load_extension('AdminCogs')
 client.load_extension('Voting')
 client.run(os.getenv('discord_bot'))
